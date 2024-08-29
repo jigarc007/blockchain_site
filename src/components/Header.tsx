c5579dad0e9a8b56478dae024c5436bc24e049db
jigarc007 <jigarv007@gmail.com>
2024-08-29 13:39:48 +0530

    remove un used line

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,7 +15,6 @@ import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 
 const drawerWidth = 240;
